886413aed5a224fe1f6e2dca8fd7db2e98236f77
Laiza <l.aiza.saraiva@gmail.com>
2023-12-05 13:33:42 -0300

    segunda versão

---
+++ desafio 2 diferente mais facil/js.js
@@ -0,0 +1,9 @@
+function troca() {
+  document.querySelector('.grupo1').style.display = 'none';
+  document.querySelector('.grupo2').style.display = 'block';
+}
+
+function volta() {
+  document.querySelector('.grupo2').style.display = 'none';
+  document.querySelector('.grupo1').style.display = 'block';
+}
